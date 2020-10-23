4f4385632e298705c76598ce81f609248ade305f
tien phan <tienphan.dev@gmail.com>
2020-10-23 04:04:29 -0400

    Set up root homepage & erb file inside views folder to render homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # landing page will be home AKA root_path
+  root 'static_pages#home'
 end
